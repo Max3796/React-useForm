d7d625a07d7df7cbb29db9f148e3d72a859cbf3b
Max3796 <gurpreet.singh@kalvium.community>
2024-03-12 10:07:53 +0530

    Done

---
+++ useForm/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+import FormUse from './Components/Forms'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <FormUse />
+    </>
+  )
+}
+
+export default App
